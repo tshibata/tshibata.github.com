7801a75a0d5526366528a40d294323e0fffd888b
tshibata <tshibata@greenliff.com>
2013-06-23 10:51:27 +0200

    6.Document

---
+++ stasc/tutorial/Sxout.js
@@ -0,0 +1,40 @@
+(function() {
+	var lastElement;
+	var lastComment;
+	var f = function(o) {
+		if (o.nodeType == 1) {
+			lastElement = o;
+		} else if (o.nodeType == 8) {
+			lastComment = o;
+		}
+		for (var i = 0; i < o.childNodes.length; i++) {
+			f(o.childNodes[i]);
+		}
+	};
+	var h = function(o) {
+		var container = window[o.nodeType]();
+		for (var key in o) {
+			if (key !== "nodeType") {
+				switch (Object.prototype.toString.call(o[key])) {
+				case "[object Number]":
+				case "[object String]":
+					container[key] = o[key];
+					break;
+				case "[object Array]":
+					for (var i = 0; i < o[key].length; i++) {
+						container[key].appendChild(h(o[key][i]));
+					}
+					break;
+				default:
+					container[key] = h(o[key]);
+					break;
+				}
+			}
+		}
+		return container;
+	};
+	f(document);
+	var element = h(JSON.parse(lastComment.nodeValue));
+	lastElement.parentNode.insertBefore(element, lastElement);
+})();
+
