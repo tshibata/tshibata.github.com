0f36699a4ef921bf870fe43a0c098ff938f8cb6b
tshibata <tshibata@greenliff.com>
2013-11-03 12:06:13 +0100

    update firefox package

--- stasc/paraboloid/package.sh
+++ stasc/paraboloid/package.sh
@@ -4,6 +4,7 @@ rm -rf package.zip package
 mkdir package
 convert -resize 32x32 icon.svg package/icon32.png
 convert -resize 60x60 icon.svg package/icon60.png
+convert -resize 64x64 icon.svg package/icon64.png
 convert -resize 90x90 icon.svg package/icon90.png
 convert -resize 120x120 icon.svg package/icon120.png
 convert -resize 128x128 icon.svg package/icon128.png
